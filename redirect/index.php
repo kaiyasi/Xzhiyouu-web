e371717c24e202d5db4c22621fcbb4ad91218ea7
xzhiyouu <nelson970602@gmail.com>
2025-05-25 14:48:46 +0800

    Add files via upload

---
+++ redirect/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Get the Flag</title>
+    <script>
+        function getFlag() {
+            window.location.href = 'flag.php';
+        }
+    </script>
+</head>
+<body>
+    <h1>Welcome to NHISC CTF!</h1>
+    <button onclick="getFlag()">GET FLAG</button>
+</body>
+</html>
